6e059d890c26769b96860dc319e7147a110f9ed1
冇文化 <261345723@qq.com>
2025-06-20 16:40:16 +0800

    - BACKEND_API_BASE_URL 设置为http://xxxxx

--- api/hello.js
+++
@@ -1,5 +0,0 @@
-// api/hello.js
-export default function handler(req, res) {
-  console.log('[HELLO LOG] Hello function invoked! Request URL:', req.url);
-  res.status(200).send('Hello from Vercel Serverless Function!');
-}
